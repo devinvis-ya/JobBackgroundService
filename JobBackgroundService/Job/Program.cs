b0a757617f6b1ba289ec68d053581d2dcf3b3d53
Devinvis <devinvis@yandex.ru>
2020-02-09 20:55:44 +0300

    New .net core console app

---
+++ JobBackgroundService/Job/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
