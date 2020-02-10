fc1ed1745ee62d91af48ee310880c6dac8baece9
Devinvis <devinvis@yandex.ru>
2020-02-10 23:40:02 +0300

    add Helper for Get config

---
+++ JobBackgroundService/Job/Helper/ConfigurationHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Job.Settings;
+using Microsoft.Extensions.Configuration;
+
+namespace Job.Helper
+{
+    public static class ConfigurationHelper
+    {
+        public static AppSettings GetAppSettings(IConfiguration conf)
+        {
+            return conf.GetSection("AppSettings")?.Get<AppSettings>();
+        }
+    }
+}
