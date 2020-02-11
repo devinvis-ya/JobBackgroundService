fe8e38dd9ab44eee67aa539786dcdb5e38830a31
Devinvis <devinvis@yandex.ru>
2020-02-12 01:23:40 +0300

    Add UriHelper to add some parameters to uri

---
+++ JobBackgroundService/Job/Helper/UriHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace Job.Helper
+{
+    public static class UriHelper
+    {
+        public static Uri AddParameter(this Uri url, string paramName, string paramValue)
+        {
+            var uriBuilder = new UriBuilder(url);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query[paramName] = paramValue;
+            uriBuilder.Query = query.ToString();
+
+            return uriBuilder.Uri;
+        }
+    }
+}
